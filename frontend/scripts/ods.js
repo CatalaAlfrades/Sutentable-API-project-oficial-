ef09c7cba59d85fc1433f85f3f297ecefe203988
CatalaAlfrades <132499405+CatalaAlfrades@users.noreply.github.com>
2025-02-08 08:36:54 -0400

    Update ods.js

--- frontend/scripts/ods.js
+++ frontend/scripts/ods.js
@@ -1,4 +1,4 @@
-const API_URL = "https://sutentable-api-project-oficial.onrender.com";
+const API_URL = "https://sutentable-api-project-oficial.onrender.com/api/actions";
 
 document.addEventListener("DOMContentLoaded", function () {
     const actionForm = document.getElementById("actionForm");
